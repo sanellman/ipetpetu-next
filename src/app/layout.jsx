71c5f02214ab8b51a5dafb43a3741e2a9d70d461
SanellmanL <pattanapong.saman@gmail.com>
2025-08-15 10:03:14 +0700

    add icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "ipetpetu",
   description: "Petpet Euphonie",
+  icons: {
+    icon: '/images/icons/icon1.png',
+  },
 };
 
 export default function RootLayout({ children }) {
